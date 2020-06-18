180ea8ef87e592a6222ce65e9a26bd9fef9f4215
Nicolás Riquelme <nrriquelme@gmail.com>
2020-06-17 23:05:10 -0400

    WIP: testing util to validate body II

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,7 +1,16 @@
 package utils
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestExtractUrlToProcess(t *testing.T) {
+	urlToTest := "some/url/to/test"
+	method := "POST"
+	endpointDescriptor := "expenses"
 
+	r1, r2 := ExtractUrlToProcess(urlToTest, method, endpointDescriptor)
+
+	fmt.Println(r1, r2)
 }
