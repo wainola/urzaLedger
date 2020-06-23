93358e7925bf60f731082701758104f3d7e44d40
Nicolás Riquelme <nrriquelme@gmail.com>
2020-06-22 22:22:13 -0400

    Adding test for the id extractor

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,16 +1,38 @@
 package utils
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestExtractUrlToProcess(t *testing.T) {
-	urlToTest := "some/url/to/test"
-	method := "POST"
-	endpointDescriptor := "expenses"
+	t.Run("Should return the id for a url with one id as a parameter", func(t *testing.T) {
+		urlToTest := "api/incomes/ABC123"
+		method := "post"
+		endpointDescriptor := "incomes"
 
-	r1, r2 := ExtractUrlToProcess(urlToTest, method, endpointDescriptor)
+		_, r2 := ExtractUrlToProcess(urlToTest, method, endpointDescriptor)
 
-	fmt.Println(r1, r2)
+		if r2 != "ABC123" {
+			t.Errorf("Error: expected %s => have %s", "ABC123", r2)
+		}
+	})
+
+	t.Run("Should return the ids of the url", func(t *testing.T) {
+		url := "api/incomes/ABC123/income/123XYZ"
+		method := "put"
+		endpoint := "incomes"
+
+		r1, _ := ExtractUrlToProcess(url, method, endpoint)
+
+		first := r1[0]
+		second := r1[1]
+
+		if first != "ABC123" {
+			t.Errorf("Error: expected ABC123 => have %s", first)
+		}
+
+		if second != "123XYZ" {
+			t.Errorf("Error: expected 123XYZ => have %s", second)
+		}
+	})
 }
